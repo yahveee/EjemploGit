a86e007c7354b94d87402664248cca55c2eee602
yahveee <groal_1992@hotmail.com>
2015-11-06 19:17:24 +0100

    Finalizada la piramide
    Falta darla la vuelta

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,4 +1,24 @@
+import acm.program.*;
+import acm.graphics.*;
 
-public class Ejercicio1 {
+public class Ejercicio1 extends acm.program.GraphicsProgram{
 
+	public void run(){
+		//cambio el tamaño de la ventana
+		setSize(800, 600);
+		//llamo al metodo que dibuja la piramide
+		pintaPiramide();
+	}
+	public void pintaPiramide(){
+		int numLadrilloBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
+		for(int j=0; j< numLadrilloBase; j ++){
+			for(int i=j; i< numLadrilloBase; i++){
+				GRect ladrillo = new GRect(anchoLadrillo, altoLadrillo);
+				add(ladrillo, anchoLadrillo*j/-2 + (anchoLadrillo * i)*-1, (altoLadrillo * j)*-1);
+
+			}
+		}
+	}
 }
